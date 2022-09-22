69ebc6d1be1f9fd74dca64a9ea92f47d5e9cbbcd
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2022-09-22 12:19:59 +0200

    handle push notifications

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -2,14 +2,39 @@
 
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 
 import '../widgets/messages/messages.dart';
 import '../widgets/messages/new_message.dart';
 
-class ChatScreen extends StatelessWidget {
+class ChatScreen extends StatefulWidget {
   const ChatScreen({Key key}) : super(key: key);
 
   static const logout = 'logout';
+
+  @override
+  _ChatScreenState createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+
+  @override
+  void initState() {
+    super.initState();
+    final fbm = FirebaseMessaging();
+    fbm.requestNotificationPermissions();
+    fbm.configure(onMessage: (msg) {
+      print(msg);
+      return;
+    }, onLaunch: (msg) {
+      print(msg);
+      return;
+    }, onResume: (msg) {
+      print(msg);
+      return;
+    }
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -35,7 +60,7 @@ class ChatScreen extends StatelessWidget {
               )
             ],
             onChanged: (itemIdentifier) {
-              if (itemIdentifier == logout) {
+              if (itemIdentifier == ChatScreen.logout) {
                 FirebaseAuth.instance.signOut();
               }
             },
