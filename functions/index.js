ae9ef72a1e2e3445ff58eb6b01ce81357598b9fe
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2022-09-22 13:24:51 +0200

    send push notifications

--- functions/index.js
+++ functions/index.js
@@ -1,18 +1,19 @@
 const functions = require("firebase-functions");
-const admin = require('firebase-admin');
+const admin = require("firebase-admin");
+
+admin.initializeApp();
+
 exports.myFunction = functions.firestore
     .document("chat/{message}")
     .onCreate((snap, context) => {
       return admin
-        .messaging()
-        .sendToTopic('chat', {
+          .messaging()
+          .sendToTopic("chat", {
             notification: {
-                title: snap.data().username,
-                body: snapshot.data().text
-                clickAction: 'FLUTTER_NOTIFICATION_CLICK'
-              }
-            }
-         )
-
-      return;
+              title: snap.data().userName,
+              body: snap.data().text,
+              clickAction: "FLUTTER_NOTIFICATION_CLICK",
+            },
+          }
+          );
     });
