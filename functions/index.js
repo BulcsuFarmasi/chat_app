bb70893c6ebd40a91f06456cbab50f189720defc
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2022-09-22 13:04:18 +0200

    set up firebase function

--- functions/index.js
+++ functions/index.js
@@ -3,7 +3,16 @@ const admin = require('firebase-admin');
 exports.myFunction = functions.firestore
     .document("chat/{message}")
     .onCreate((snap, context) => {
-      console.log(snap.data());
+      return admin
+        .messaging()
+        .sendToTopic('chat', {
+            notification: {
+                title: snap.data().username,
+                body: snapshot.data().text
+                clickAction: 'FLUTTER_NOTIFICATION_CLICK'
+              }
+            }
+         )
 
       return;
     });
